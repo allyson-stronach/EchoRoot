3f23957b93ad489c46c81490d57298db7138fd59
Allyson Stronach <allyson.stronach@gmail.com>
2014-11-19 13:18:20 -0800

    made changes based on code refive with jessica. also started prettifying website

--- adanalysis.py
+++ adanalysis.py
@@ -9,12 +9,22 @@ from sklearn import metrics
 from model import session
 
 
+def analyze_ad():
+    dl_list = retrieve_trafficky_text()
+    dl_list = retrieve_not_trafficky_text(dl_list)
+    vectorizer = TfidfVectorizer()
+    x_axis_y_axis = vectorize_ads(dl_list, vectorizer)
+    classifier = classify_ads(x_axis_y_axis)
+    test_sample(vectorizer, classifier)
+    describe_features(vectorizer, classifier)
+
+
 def retrieve_trafficky_text():
     documents = []
     labels = []
     ads_text_cmd = "SELECT ads.text AS text FROM ads_attributes JOIN ads ON ads.id = ads_id WHERE ads_attributes.value IN  ('9292103206', '4142395461', '4146870501') "
-    e = session.execute(ads_text_cmd)
-    for text in e:
+    trafficky_text = session.execute(ads_text_cmd)
+    for text in trafficky_text:
         documents.append(text.text)
         labels.append('trafficky')
     print 'documents length:', len(documents), 'labels length:', len(labels)
@@ -25,8 +35,8 @@ def retrieve_trafficky_text():
 
 def retrieve_not_trafficky_text(dl_list):
     ads_text_cmd = "SELECT ads.text AS text FROM ads_attributes JOIN ads ON ads.id = ads_id WHERE ads_attributes.value IN ('3104623985', '2139840845 ', '8183362736', '6032946322', '4088991922')"
-    e = session.execute(ads_text_cmd)
-    for text in e:
+    not_trafficky_text = session.execute(ads_text_cmd)
+    for text in not_trafficky_text:
         dl_list[0].append(text.text)
         dl_list[1].append('not trafficky')
     print 'documents length:', len(dl_list[0]), 'labels length:', len(dl_list[1])
@@ -34,65 +44,55 @@ def retrieve_not_trafficky_text(dl_list):
 
     return dl_list
 
-def instantiate_vectorizer():
-    vectorizer = TfidfVectorizer()
-
-    return vectorizer
 
 def vectorize_ads(dl_list, vectorizer):
     X = vectorizer.fit_transform(dl_list[0])
     y = np.array(dl_list[1])
     print 'X.shape is:', X.shape, 'y.shape is:', y.shape, 'vectorizer:', vectorizer
-    xy = [X, y]
+    x_axis_y_axis = [X, y]
 
-    return xy
+    return x_axis_y_axis
 
 
-def classify_ads(xy):
-    clf = BernoulliNB()
-    cv = cross_validation.StratifiedKFold(xy[1],5)
+def classify_ads(x_axis_y_axis):
+    classifier = BernoulliNB()
+    cv = cross_validation.StratifiedKFold(x_axis_y_axis[1],5)
     precision=[]
     recall=[]
     for train, test in cv:
-        X_train = xy[0][train]
-        X_test = xy[0][test]
-        y_train = xy[1][train]
-        y_test = xy[1][test]
-        clf.fit(X_train, y_train)
-        y_hat = clf.predict(X_test)
+        X_train = x_axis_y_axis[0][train]
+        X_test = x_axis_y_axis[0][test]
+        y_train = x_axis_y_axis[1][train]
+        y_test = x_axis_y_axis[1][test]
+        classifier.fit(X_train, y_train)
+        y_hat = classifier.predict(X_test)
         p,r,_,_ = metrics.precision_recall_fscore_support(y_test, y_hat)
         precision.append(p[1])
         recall.append(r[1])
-    print clf
+    print classifier
     print 'precision:',np.average(precision), '+/-', np.std(precision)
     print 'recall:', np.average(recall), '+/-', np.std(recall)
     
-    return clf
+    return classifier
 
 
-def test_sample(vectorizer, clf):
+def test_sample(vectorizer, classifier):
     sample = 'Long hair... Long Legs Tall, Busty , Beautiful, Luscious Lips and Curvy Hips<br> All Service<br> In or Out Call<br> Available Days and Nights<br> call 336 307 5841 |'
     sample = vectorizer.transform([sample])
-    c = clf.predict(sample)
+    c = classifier.predict(sample)
     
     print c
 
 
-def describe_features(vectorizer, clf):
-    probs=clf.feature_log_prob_[1]
+def describe_features(vectorizer, classifier):
+    probs=classifier.feature_log_prob_[1]
     features=vectorizer.get_feature_names()
     
     print 'length of probs:', len(probs), 'length of features:', len(features), 'list of most important features:', sorted(zip(probs,features), reverse=True)[:10]
 
 
 def main():
-    dl_list = retrieve_trafficky_text()
-    dl_list = retrieve_not_trafficky_text(dl_list)
-    vectorizer = instantiate_vectorizer()
-    xy = vectorize_ads(dl_list, vectorizer)
-    clf = classify_ads(xy)
-    test_sample(vectorizer, clf)
-    describe_features(vectorizer, clf)
+    analyze_ad()
 
 if __name__ == "__main__":
     main()
--- website.py
+++ website.py
@@ -6,7 +6,15 @@ app.secret_key = "7mghJqRp^tU1tE-me^A8TTPF]$Rr$;~gQYmM<8Zq{b7(f`BQ79bC.-/rB07T"
 
 @app.route("/")
 def index():
-    pass
+	ad_number = '1,000,000'
+	att_number = '3,500,000'
+	return render_template('index.html', ad_number=ad_number, att_number=att_number)
+
+@app.route('/analyze', methods = ['POST'])
+def signup():
+    email = request.form['adform']
+    print("The text in the ad you submitted is '" + new_ad + "'")
+    return redirect('/')
 
 if __name__== "__main__":
     app.run(debug = True)
